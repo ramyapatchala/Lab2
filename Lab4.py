ec9252afbf0140116f45930a9e5b5aaaaebe1913
ramyapatchala <rpatchal@syr.edu>
2024-09-19 12:06:39 -0400

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -64,7 +64,7 @@ if st.sidebar.button("Search"):
                         model="text-embedding-3-small")
         query_embedding = response.data[0].embedding
         results = collection.query(
-                    query_embeddings=[query_embedding],
+                    query_texts= [topic],
                     n_results=3)
         for i in range(len(results['ids'][0])):
             doc_id = results['ids'][0][i]
