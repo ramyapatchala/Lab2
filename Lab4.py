afea413f979641a4221fd92171793173e5a48a37
ramyapatchala <rpatchal@syr.edu>
2024-09-19 11:29:24 -0400

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -2,10 +2,10 @@ import streamlit as st
 from openai import OpenAI
 import os
 from PyPDF2 import PdfReader
-import importlib
-importlib.import_module('pysqlite3')
+__import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import chromadb
 
 if 'openai_client' not in st.session_state:
