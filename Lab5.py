8f42b7640feed5941b9679839a6c7d818143d553
ramyapatchala <rpatchal@syr.edu>
2024-09-26 11:03:01 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,1 +1,26 @@
-
+import requests
+import streamlit as st
+def get_current_weather(location, API_key):
+    if "," in location:
+      location = location.split(",")[0].strip()
+      
+    urlbase = "https://api.openweathermap.org/data/2.5/"
+    urlweather = f"weather?q={location}&appid={API_key}"
+    url = urlbase + urlweather
+    response = requests.get(url)
+    data = response.json()
+    # Extract temperatures & Convert Kelvin to Celsius
+    temp = data['main']['temp'] - 273.15
+    feels_like = data['main']['feels_like'] - 273.15
+    temp_min = data['main']['temp_min'] - 273.15
+    temp_max = data['main']['temp_max'] - 273.15
+    humidity = data['main']['humidity']
+    return {"location": location,
+            "temperature": round(temp, 2),
+            "feels_like": round(feels_like, 2),
+            "temp_min": round(temp_min, 2),
+            "temp_max": round(temp_max, 2),
+            "humidity": round(humidity, 2)
+            }
+api_key = st.secrets["OpenWeatherAPIkey"]
+st.write(get_current_weather('Syracuse', api_key)
